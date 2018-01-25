1eb2b756347abea0f9812ba86b8272b86e685cf2
greenRose2018 <brifun13@gmail.com>
2018-01-25 09:17:25 -0700

    created a for loop in start()

--- miniProject/Controller/Controller.cpp
+++ miniProject/Controller/Controller.cpp
@@ -17,4 +17,9 @@ Controller:: Controller()
 
 void Controller:: start()
 {
+    for(int index = 0; index < 10; index+=1)
+    {
+        cout << "This loop has execucted " << index + 1 << " times" << endl;
+    }
+}
 }
